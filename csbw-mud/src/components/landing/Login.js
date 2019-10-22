8a1baede8817620b1cf7007815650bb0892e2ff0
Zurglots <bfeole@gmail.com>
2019-10-22 18:59:30 -0400

    Registration form test data working

--- csbw-mud/src/components/landing/Login.js
+++ csbw-mud/src/components/landing/Login.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Login = () => {
+const Login = props => {
+  // LOGIN HOOK
   const [loginState, setLoginState] = useState({ username: '', password: '' });
-  // const [value, setValue] = useState('');
+
+  // REGISTER HOOK
+  const [registerState, setRegisterState] = useState({
+    username: '',
+    password1: '',
+    password2: '',
+  });
+
+  const registerChange = e => {
+    setRegisterState({
+      ...registerState,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const loginChange = e => {
     setLoginState({
@@ -13,6 +27,8 @@ const Login = () => {
   };
 
   console.log('USER:', loginState);
+  console.log('USER:', registerState);
+
   // console.log('VALUE:', value);
 
   // const handleSubmit = e => {
@@ -24,34 +40,83 @@ const Login = () => {
   const handleLogin = e => {
     e.preventDefault();
     axios
-      .post('https://lambda-mud-test.herokuapp.com/api/login', loginState)
+      .post('https://lambda-mud-test.herokuapp.com/api/login/', loginState)
       .then(res => {
+        localStorage.setItem('token', res.data.key);
+        // props.setLoginState(true);
         console.log(res.data);
       })
       .catch(err => {
+        console.error(err);
+        console.log(err);
+      });
+  };
+
+  const handleRegister = e => {
+    e.preventDefault();
+    axios
+      .post(
+        'https://lambda-mud-test.herokuapp.com/api/registration/',
+        registerState,
+      )
+      .then(res => {
+        localStorage.setItem('token', res.data.key);
+        // props.setLoginState(true);
+        console.log('REGISTER DATA', res.data);
+      })
+      .catch(err => {
+        console.error(err);
         console.log(err);
       });
   };
 
   return (
-    <div className='login-wrapper'>
-      <form onSubmit={handleLogin}>
-        <div className='input-fields'>
-          <input
-            onChange={loginChange}
-            name='username'
-            placeholder='username'
-            value={loginState.username}
-          />
-          <input
-            onChange={loginChange}
-            placeholder='password'
-            name='password'
-            value={loginState.password}
-          />
-          <button type='submit'>Log in</button>
-        </div>
-      </form>
+    <div className='main-wrapper'>
+      <div className='login-wrapper'>
+        <form onSubmit={handleLogin}>
+          <div className='input-fields'>
+            <input
+              placeholder='username'
+              onChange={loginChange}
+              name='username'
+              value={loginState.username}
+            />
+            <input
+              onChange={loginChange}
+              placeholder='password'
+              name='password'
+              value={loginState.password}
+            />
+            <button type='submit'>Log In</button>
+          </div>
+        </form>
+      </div>
+
+      <div className='register-form'>
+        <form onSubmit={handleRegister}>
+          <div className='input-fields'>
+            <input
+              placeholder='username'
+              onChange={registerChange}
+              name='username'
+              value={registerState.username}
+            />
+            <input
+              onChange={registerChange}
+              placeholder='password'
+              name='password1'
+              value={registerState.password1}
+            />
+            <input
+              onChange={registerChange}
+              placeholder='confirm password'
+              name='password2'
+              value={registerState.password2}
+            />
+            <button type='submit'>Sign Up</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
