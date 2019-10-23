5f78b530f042c0c4d7d4e09e3b64eef8e7cd460c
Zurglots <bfeole@gmail.com>
2019-10-22 21:45:49 -0400

    Updated init endpoint working
    Co-authored-by: Derek Schwantner <dschwan21@me.com>

--- csbw-mud/src/App.js
+++ csbw-mud/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Landing from './components/landing/Landing';
+import Game from './components/game/Game';
 
 function App() {
   return (
     <div className='main-wrapper-app'>
       <div className='component-wrapper'>
-        <Landing />
+        {localStorage.getItem('token') ? <Game /> : <Landing />}
       </div>
     </div>
   );
--- csbw-mud/src/components/game/game.js
+++ csbw-mud/src/components/game/game.js
@@ -2,34 +2,52 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Game = props => {
-  [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
-  const current_user_token = localStorage.getItem('token');
+  const current_user_token = localStorage.token;
+  console.log('token var:', current_user_token);
 
-  useEffect => () => {
-    const fetchData = async () => {
-      if (current_user_token) {
-        try {
-          axios
-            .get(`https://lambda-mud-test.herokuapp.com/api/adv/init`, {
-              Authorization: current_user_token,
-            })
-            .then(res => {
-              setUserData(res.data);
-              console.log('REGISTER DATA', res.data);
-            });
-        } catch {}
-      }
-    };
-  },
-    [current_user_token, set];
+  useEffect(() => {
+    // const fetchData = async () => {
+    // if (localStorage.getItem('token')) {
+    //   try {
+    //     let response = await
+    setIsLoading(true);
+    axios
+      .get('https://lambda-mud-test.herokuapp.com/api/adv/init/', {
+        headers: {
+          Authorization: `Token ${current_user_token}`,
+        },
+      })
+      // console.log('res var:', res);
+      // setUserData(res.data);
+      // console.log('REGISTER DATA', res.data);
+      .then(res => {
+        setUserData({ name: res.data.name });
+        setIsLoading(false);
+        console.log('REGISTER DATA', res.data);
+      })
+      // .catch(err => {
+      //   console.log(err);
+      // });
+      // }
+      .catch(err => {
+        console.log('CATCH from TRY', err);
+        setIsLoading(false);
+      });
+  }, []);
+  // fetchData();
+  // current_user_token, userData
 
+  console.log('userdata:', userData);
   return (
     <div>
       {/* We can render individual Game components from here
         maps, movement, room info, etc. from another main
         component */}
       <p>main Game container</p>
+      {/* <Map /> */}
     </div>
   );
 };
