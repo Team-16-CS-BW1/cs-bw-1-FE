d78f036891cdd8e126bf29596dca969c799ae6b0
Zurglots <bfeole@gmail.com>
2019-10-23 16:28:29 -0400

    Bug chasing deploy

--- csbw-mud/src/App.js
+++ csbw-mud/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Landing from './components/landing/Landing';
+import Main from './components/landing/Main';
 import Game from './components/game/Game';
 
 function App() {
   return (
     <div className='main-wrapper-app'>
       <div className='component-wrapper'>
-        {localStorage.getItem('token') ? <Game /> : <Landing />}
+        {localStorage.getItem('token') ? <Game /> : <Main />}
       </div>
     </div>
   );
